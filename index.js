084d6e66db7b6e6fc17c0340c8023572f40d934c
allendlg <allen.delagarza01@gmail.com>
2021-07-05 21:43:31 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased(){
+  const capTutorials = tutorials.map(t => {
+    const words = t.split(" ").map(word => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    }).join(" ");
+    console.log(words);
+    return words; 
+  })
+  return capTutorials;
 }
